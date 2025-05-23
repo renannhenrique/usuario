8f785169d418fa611ec4ec060e1ad5964fcbc11a
Rhe <renanhenriquehp@gmail.com>
2025-05-23 15:09:31 -0300

    commit inicial

---
+++ src/test/java/com/rhedeveloper/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhedeveloper.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
