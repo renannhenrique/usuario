0655920788ac2c9d99e68e801f068fdd865d6d4d
Renan Henrique <contatorenandeveloper@gmail.com>
2025-05-26 09:20:36 -0300

    adicionados métodos get, delete e login

--- src/main/java/com/rhedeveloper/usuario/controller/UsuarioController.java
+++ src/main/java/com/rhedeveloper/usuario/controller/UsuarioController.java
@@ -2,30 +2,52 @@ package com.rhedeveloper.usuario.controller;
 
 import com.rhedeveloper.usuario.business.UsuarioService;
 import com.rhedeveloper.usuario.business.dto.UsuarioDTO;
-import lombok.Getter;
+import com.rhedeveloper.usuario.infrastructure.entity.Usuario;
+import com.rhedeveloper.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@Getter
-@Setter
-@RequiredArgsConstructor
 @RequestMapping("/usuario")
+@RequiredArgsConstructor
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
-    private final UsuarioDTO usuarioDTO;
-
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     //Método para salvar usuário
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO){
         return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
     }
+
+    //Método Get
+    @GetMapping
+    public ResponseEntity<Usuario> buscaUsuarioPorEmail(@RequestParam("email") String email){
+        return ResponseEntity.ok(usuarioService.buscaUsuarioPorEmail(email));
+    }
+
+    //Método Login
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDTO usuarioDTO){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(),
+                        usuarioDTO.getSenha())
+        );
+        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+    }
+
+    //Excluir usuário
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deletaUsuarioPorEmail(@PathVariable String email){
+        usuarioService.deletaUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+    }
+
 
 }
--- src/main/java/com/rhedeveloper/usuario/infrastructure/repository/TelefoneRepository.java
+++ src/main/java/com/rhedeveloper/usuario/infrastructure/repository/TelefoneRepository.java
@@ -1,6 +1,6 @@
 package com.rhedeveloper.usuario.infrastructure.repository;
 
-import com.javanauta.aprendendo_spring.infrastructure.entity.Telefone;
+import com.rhedeveloper.usuario.infrastructure.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
